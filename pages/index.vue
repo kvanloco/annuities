f8e923879a1e85cd6768152313ba667d61a8623e
Kris Van Loco <kvanloco@gmail.com>
2024-08-27 14:49:30 +0000

    abc

--- pages/index.vue
+++ pages/index.vue
@@ -13,89 +13,101 @@
       <label for="duration">Duration in years</label>
       <UInput v-model="duration" />
     </div>
+    boe {{ annuityResult }}
     <div class="my-5">
       <UButton @click="calculate">Calculate</UButton>
     </div>
     <UDivider />
   </div>
-
-  <h1 class="text-3xl font-bold underline">Input parameters</h1>
-  <UTable :rows="inputParameterTable" />
-  <h1 class="text-3xl font-bold underline">Annuity info</h1>
-  <UTable :rows="annuityInfoTable" />
-  <h1 class="text-3xl font-bold underline">Annuity table</h1>
-  <UTable :rows="annuity.annuity_table" />
+  <div v-if="annuityResult">
+    <h1 class="text-3xl font-bold underline">Input parameters</h1>
+    <UTable :rows="inputParameterTable" />
+    <h1 class="text-3xl font-bold underline">Annuity info</h1>
+    <UTable :rows="annuityInfoTable" />
+    <h1 class="text-3xl font-bold underline">Annuity table</h1>
+    <UTable :rows="annuityTable" />
+  </div>
 </template>
 
 <script setup lang="ts">
 const amount = ref(100000);
 const rate = ref(0.04);
 const duration = ref(10);
+const annuityResult = ref(null);
 
-const calculate = () => {
-  console.log(amount.value, rate.value, duration.value);
+const calculate = async () => {
+  const annuityResult = await $fetch("/api/annuity", {
+    method: "POST",
+    body: {
+      amount: amount.value,
+      rate: rate.value,
+      duration: duration.value,
+    },
+  });
+  console.log("annuityResult", annuityResult);
+  annuityResult.value = toRef(annuityResult); // annuityResult;
 };
 
-const { data: annuity, error } = await useFetch("/api/annuity", {
-  method: "POST",
-  body: {
-    amount: 100000,
-    rate: 0.04,
-    duration: 10,
-  },
+watch(
+  () => annuityResult,
+  () => {
+    console.log("watcheffect");
+    !annuityResult.value ? [] : annuityResult.value.annuity_table;
+  }
+);
+
+const annuityTable = computed(() => {
+  if (!annuityResult.value) return [];
+  return annuityResult.value.annuity_table;
 });
 
 const inputParameterTable = computed(() => {
+  console.log("ann", annuityResult.value);
+  if (!annuityResult.value) return [];
   return [
     {
       parameter: "Loan amount",
-      value: annuity.value.input_parameters.loan_amount,
+      value: annuityResult.value.input_parameters.loan_amount,
     },
     {
       parameter: "Periods years",
-      value: annuity.value.input_parameters.periods_years,
+      value: annuityResult.value.input_parameters.periods_years,
     },
     {
       parameter: "Periods months",
-      value: annuity.value.input_parameters.periods_years,
+      value: annuityResult.value.input_parameters.periods_years,
     },
     {
       parameter: "Interest rate year",
-      value: annuity.value.input_parameters.interest_rate_year,
+      value: annuityResult.value.input_parameters.interest_rate_year,
     },
     {
       parameter: "Interest rate month",
-      value: annuity.value.input_parameters.interest_rate_month,
+      value: annuityResult.value.input_parameters.interest_rate_month,
     },
   ];
 });
 
 const annuityInfoTable = computed(() => {
+  console.log("ann2", annuityResult.value);
+  if (!annuityResult.value) return [];
   return [
     {
       parameter: "Annuity per month",
-      value: annuity.value.annuity_info.annuity_per_month,
+      value: annuityResult.value.annuity_info.annuity_per_month,
     },
     {
       parameter: "Annuity per year",
-      value: annuity.value.annuity_info.annuity_per_year,
+      value: annuityResult.value.annuity_info.annuity_per_year,
     },
     {
       parameter: "Interest cost",
-      value: annuity.value.annuity_info.interest_cost,
+      value: annuityResult.value.annuity_info.interest_cost,
     },
     {
       parameter: "Total payoff",
-      value: annuity.value.annuity_info.total_payoff,
+      value: annuityResult.value.annuity_info.total_payoff,
     },
   ];
 });
-
-if (error.value) {
-  console.error(error.value);
-}
-
-console.log(annuity.value);
 </script>
-
-brendaa_h@yahoo.com
--- server/api/annuity.ts
+++ server/api/annuity.ts
@@ -1,4 +1,4 @@
-import { calculateAnnuityPayment } from "../utils/annuity";
+import { calculateAnnuityPayment } from "../utils/annuityCalculator";
 
 type Body = {
   amount: number;
@@ -10,6 +10,11 @@ export default defineEventHandler<{ body: Body }>(async (event) => {
   const { amount, duration, rate } = await readBody(event);
   console.log(amount, duration, rate);
   //const annuity = amount * (rate / (1 - Math.pow(1 + rate, -duration)));
-  const annuity = calculateAnnuityPayment(amount, rate, duration);
+  const annuity = calculateAnnuityPayment(
+    Number(amount),
+    Number(rate),
+    Number(duration)
+  );
+
   return annuity;
 });
