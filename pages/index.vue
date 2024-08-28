d55cfa42f243e58a47dfa24452ab473294ba4f4b
Kris Van Loco <kvanloco@gmail.com>
2024-08-28 11:54:53 +0000

    boe

--- pages/index.vue
+++ pages/index.vue
@@ -17,10 +17,10 @@
       <label for="duration">Start date</label>
       <UInput type="date" v-model="start_date" />
     </div>
-    kieke
     <div class="my-5">
       <UButton @click="calculate">Calculate</UButton>
     </div>
+    {{ start_date }}
     <UDivider />
   </div>
   <div v-if="annuityResult">
@@ -68,7 +68,7 @@
         <span>{{ toEuro.format(row.total_payed) }}</span>
       </template>
       <template #time-data="{ row }">
-        <span>{{ toDateMonth.format(new Date(row.time)) }}</span>
+        <span>{{ toDate.format(new Date(row.time)) }}</span>
       </template>
     </UTable>
   </div>
@@ -86,16 +86,24 @@ let toPercent = new Intl.NumberFormat("be-BE", {
   maximumSignificantDigits: 3,
 });
 
-let toDateMonth = new Intl.DateTimeFormat("be-BE", {
+let toDate = new Intl.DateTimeFormat("be-BE", {
   dateStyle: "short",
 });
 
 const amount = ref(100000);
 const rate = ref(4);
 const duration = ref(10);
-const start_date = ref(null);
+const start_date2 = ref("2018-04-01");
+const start_date = ref("28-8-2024");
 const annuityResult = ref(undefined);
-
+console.log(start_date);
+console.log(start_date2);
+console.log("========");
+console.log(new Date().toISOString());
+console.log(new Date("2018-04-01").toLocaleString("be-BE"));
+console.log(Date.now());
+console.log(new Date().toLocaleDateString());
+console.log(Date.parse("2018-04-01"));
 const calculate = async () => {
   const annuityResults = await $fetch("/api/annuity", {
     method: "POST",
