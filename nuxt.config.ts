b05f194945e0ec284bd17b662533b40c6e25e758
Kris Van Loco <kvanloco@gmail.com>
2025-02-12 11:58:57 +0000

    gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@vueuse/nuxt", "@pinia/nuxt"],
+  modules: ["@nuxt/ui", "@vueuse/nuxt", "@pinia/nuxt", "nuxt-gtag"],
   compatibilityDate: "2024-08-27",
+  gtag: {
+    id: "G-LYKQ3GTX77",
+  },
 });
